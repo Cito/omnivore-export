13037f8243012d0d8f806dbf1218d86aca44153f
Christoph Zwerschke <cito@online.de>
2023-09-09 17:53:04 +0200

    Initial version of the script

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # omnivore-export
-Export tool for the Omnivore read-it-later solution
+
+This is an export tool for the
+[Omnivore](https://github.com/omnivore-app/omnivore)
+read-it-later solution written in Python.
+
+Its main purpose is to create a backup of all links saved in Omnivore,
+as long as the Omnivore app stil lacks an export function.
+
+To run this tool, you need to install
+[gql](https://github.com/graphql-python/gql) with httpx support first:
+
+```sh
+pip install --pre -U gql[httpx]
+```
+
+Next, you must
+[create an API key for Omnivore](https://omnivore.app/settings/api).
+
+Then, change the global variable `api_key`, or set the environment variable
+`OMNIVORE_API_KEY`.
+
+If you're not using the free hosted Omnivore, you must also change the global
+variable `api_url` or set the environment variable `OMNIVORE_API_URL`.
+
+You can change the path for the exported data with the global variable
+`backup_path` or the environment variable `OMNIVORE_BACKUP_PATH`.
---
+++ omnivore-export.py
@@ -0,0 +1,100 @@
+#!/usr/bin/python3.11
+
+"""Export Links from Omnivore.
+
+more info at https://github.com/Cito/omnivore-export
+"""
+
+from datetime import date
+from json import dump
+from os import environ
+
+from gql import gql, Client
+from gql.transport.httpx import HTTPXTransport
+
+api_url = "https://api-prod.omnivore.app/api/graphql"
+api_key = "FFFFFF-FFFF-FFFF-FFFF-FFFFFFFFFFFF"
+
+backup_path = "omnivore_backup.json"
+
+add_date_to_path = True
+
+query_all = """
+{
+    search(first: null, after: null, query: "in:all") {
+        ... on SearchSuccess {
+            edges {
+                cursor
+                node {
+                    title
+                    slug
+                    url
+                    pageType
+                    contentReader
+                    createdAt
+                    updatedAt
+                    isArchived
+                    readingProgressPercent
+                    readingProgressTopPercent
+                    readingProgressAnchorIndex
+                    author
+                    image
+                    description
+                    publishedAt
+                    ownedByViewer
+                    originalArticleUrl
+                    uploadFileId
+                    labels {
+                        name
+                    }
+                    pageId
+                    shortId
+                    quote
+                    annotation
+                    state
+                    siteName
+                    subscription
+                    readAt
+                    savedAt
+                    wordsCount
+                    content
+                    archivedAt
+                }
+            }
+        }
+        ... on SearchError {
+            errorCodes
+        }
+    }
+}
+"""
+
+
+def get_all(url, key):
+    headers = dict(Authorization=key)
+    transport = HTTPXTransport(url=url, headers=headers)
+    client = Client(transport=transport)
+    query = gql(query_all)
+    result = client.execute(query)
+    return result
+
+
+def save_backup(data, path):
+    with open(path, 'w', encoding='utf-8') as backup:
+        dump(data, backup)
+
+
+def main():
+    url = environ.get('OMNIVORE_API_URL', api_url)
+    key = environ.get('OMNIVORE_API_KEY', api_key)
+    data = get_all(url, key)
+    path = environ.get('OMNIVORE_BACKUP_PATH', backup_path)
+    if add_date_to_path:
+        parts = list(path.partition('.'))
+        parts.insert(-2, "-" + date.today().isoformat())
+        path = ''.join(parts)
+    save_backup(data, path)
+
+
+if __name__ == '__main__':
+    main()
