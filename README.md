56f7ffe1b77131a68a8db1e64edfa2c689053ea6
Christoph Zwerschke <cito@online.de>
2023-09-09 20:37:17 +0200

    Add summary script

--- README.md
+++ README.md
@@ -1,13 +1,13 @@
 # omnivore-export
 
-This is an export tool for the
+This is an export script for the
 [Omnivore](https://github.com/omnivore-app/omnivore)
 read-it-later solution written in Python.
 
 Its main purpose is to create a backup of all links saved in Omnivore,
 as long as the Omnivore app stil lacks an export function.
 
-To run this tool, you need to install
+To run this script, you need to install
 [gql](https://github.com/graphql-python/gql) with httpx support first:
 
 ```sh
@@ -17,17 +17,29 @@ pip install --pre -U gql[httpx]
 Next, you must
 [create an API key for Omnivore](https://omnivore.app/settings/api).
 
-Then, change the global variable `api_key`, or set the environment variable
-`OMNIVORE_API_KEY`.
+Then, in the script `omnivore-summary.py`,
+change the global variable `api_key`,
+or set the environment variable `OMNIVORE_API_KEY`.
 
-If you're not using the free hosted Omnivore, you must also change the global
-variable `api_url` or set the environment variable `OMNIVORE_API_URL`.
+If you're not using the free hosted Omnivore,
+you must also change the global variable `api_url`
+or set the environment variable `OMNIVORE_API_URL`.
 
-You can change the path for the exported data with the global variable
-`backup_path` or the environment variable `OMNIVORE_BACKUP_PATH`.
+You can change the path for the exported data
+with the global variable `backup_path`
+or the environment variable `OMNIVORE_BACKUP_PATH`.
 
 Finally, just run the script via Python:
 
 ```sh
 python omnivore-export.py 
+```
+
+This repository also contains a script `omnivore-summary.py`
+that can be used to print a summary of the data store in Omnivore.
+
+After configuring it in the same way as the export script, run:
+
+```sh
+python omnivore-summary.py 
 ```
---
+++ omnivore-summary.py
@@ -0,0 +1,102 @@
+#!/usr/bin/python3.11
+
+"""Export Links from Omnivore.
+
+more info at https://github.com/Cito/omnivore-export
+"""
+
+from collections import Counter
+from os import environ
+
+from gql import gql, Client
+from gql.transport.httpx import HTTPXTransport
+
+api_url = "https://api-prod.omnivore.app/api/graphql"
+api_key = "FFFFFF-FFFF-FFFF-FFFF-FFFFFFFFFFFF"
+
+
+add_date_to_path = True
+
+query_all = """
+{
+    search(first: 9999, after: null, query: "in:all") {
+        ... on SearchSuccess {
+            edges {
+                node {
+                    pageType
+                    contentReader
+                    createdAt
+                    isArchived
+                    readingProgressPercent
+                    readingProgressTopPercent
+                    readingProgressAnchorIndex
+                    labels {
+                        name
+                    }
+                    state
+                    readAt
+                    savedAt
+                }
+            }
+        }
+        ... on SearchError {
+            errorCodes
+        }
+    }
+}
+
+"""
+
+
+def get_all(url, key):
+    headers = dict(Authorization=key)
+    transport = HTTPXTransport(url=url, headers=headers)
+    client = Client(transport=transport)
+    query = gql(query_all)
+    result = client.execute(query)
+    return result
+
+
+def summarize(data):
+    num_archived = num_inbox = 0
+    page_type_counter = Counter()
+    label_counter = Counter()
+    for edge in data["search"]["edges"]:
+        node = edge["node"]
+        if node['isArchived']:
+            num_archived += 1
+        else:
+            num_inbox += 1
+        page_type = node["pageType"]
+        if page_type:
+            page_type_counter.update([page_type])
+        labels = node["labels"]
+        if labels:
+            labels = [label["name"] for label in node["labels"]]
+            label_counter.update(labels)
+
+    print()
+    print("* Inbox:", num_inbox)
+    print("* Archive:", num_archived)
+    print()
+    print("* Page types:")
+    for key, value in page_type_counter.items():
+        print(f"  - {key.capitalize()}: {value}")
+    print()
+    print("* Labels:")
+    for key, value in label_counter.items():
+        print(f"  - {key}: {value}")
+    print()
+
+
+def main():
+    url = environ.get('OMNIVORE_API_URL', api_url)
+    key = environ.get('OMNIVORE_API_KEY', api_key)
+    print("Reading data...")
+    data = get_all(url, key)
+    print("Summarizing...")
+    summarize(data)
+
+
+if __name__ == '__main__':
+    main()
